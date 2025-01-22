4e2da08423f1634f8859de048bea745b6f4d9077
Gurpreet Gandhi <64197845+grupret@users.noreply.github.com>
2025-01-22 18:55:05 +0530

    Add files via upload

---
+++ src/test/java/com/aidtaas/tekton/TektonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidtaas.tekton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
